db32895049b823624e8c62a51318f5db56b3cf3d
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2025-12-15 17:11:53 +0800

    Add files via upload

---
+++ uploadimage.js
@@ -0,0 +1,29 @@
+Create Account API Token
+app-inventor-userimage was successfully created
+Summary:
+Permissions:
+
+Allows the ability to read, write, and list objects in specific buckets.
+Buckets:All R2 buckets on this account
+Use this token for authenticating against the Cloudflare API
+:
+
+    Token value
+
+    rTDsNV5zvWt9NJTI6DEDdRMiQmhqUHHiO8F2dr9E
+
+Use the following credentials for S3 clients:
+
+    Access Key ID
+
+    61924f1cac50036134d8bea82fb6487a
+
+Secret Access Key
+
+2299fa070b5f6090480788b89ca5b50110029e906cf65fb39b73876a04a21454
+
+Use jurisdiction-specific endpoints for S3 clients:
+
+https://de2fe7999306738ad5530a2fe8d6d09b.r2.cloudflarestorage.com
+
+For security reasons, these token values will not be shown again. Learn more about API token generation
